cf20c3bb37b0c19ad267602ebb8f5af5c3fd595c
Antony-Passarin <130374591+Antony-Passarin@users.noreply.github.com>
2024-08-30 08:44:32 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,108 @@
+export const perguntas = [
+    {
+        enunciado: "De que forma a inteligencia artificial pode ser usada para beneficio proprio?",
+        alternativas: [
+            {
+                texto: "Usando de maneira preguicosa e deixando ela trabalhar por voce!",
+                afirmacao:[ 
+                    "Errado, voce nao deve utilizar a inteligencia artificial de maneira preguicosa",
+                    "Errado, e antietico utilizar ela desta forma"
+                ],
+                proxima: 1,
+                },
+            {
+                texto: "utilizar para auxilio em pesquisas e assistencia em problemas, porem ainda fazendo o trabalho sem depender totalmente dela!",
+                afirmacao: [
+                    "Correto, trabalhar em conjunto da inteligencia artificial e a maneira correta de se utiliza-la",
+                    "Correto, utilizala para acoes propriase errado mas para assistencia e ok"
+                ],
+                proxima: 2,
+            }
+        ]
+    },
+    {
+        enunciado: "Como usar de maneira etica as inteligencias artificiais!",
+        alternativas: [
+            {
+                texto: "Colocando elas para fazer suas atividades escolares ou trabalhos facultativos!",
+                afirmacao: [
+                    "Errado, utiliza-la para fazer suas atividades e com certeza uma maneira sem etica",
+                    "Errado, devesse usala corretamente"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Pedir auxilio e pesquisas sobre dado contexto para a melhor aprendizagem de si mesmo!",
+                afirmacao:[ 
+                    "Correto, Deixa-la te auxiliar durante o momento de apredizagem e uma otima utilizacao",
+                    "Correto, o auxilio dela e muito bom para um melhoramento"
+                ],
+                proxima: 4,
+            }
+        ]
+    },
+    {
+        enunciado: "A inteligencia artificial consegue programar para voce?",
+        alternativas: [
+            {
+                texto: "Claro, consegue fazer com exatidao qualquer ideia que a for pedida",
+                afirmacao: [
+                    "Errado, a IA nao consegue criar com exatidao pois o raciocinio e limitado",
+                     "Errado, a ia nao consegue realizar pensamentos criativos"
+                ],
+                proxima: 5,
+                },
+            {
+                texto: "Sim, porem limitado pois ela nunca consiguira fazer com exatidao ideias humanas",
+                afirmacao: [
+                    "Correto, pois a inteligencia artificial nunca vai conseguir emular com exatidao o cerebro humano",
+                    "Correto, pois nao consegue emular pensamentos complexos e criativos"
+                ],
+                proxima: 6,
+                },
+        ],
+    },
+    {
+        enunciado: "Quais funcoes uma IA pode executar?",
+        alternativas: [
+            {
+                texto: "Utilizada somente para coisas banais e resolucoes de problemas simples",
+                afirmacao: [
+                    "ERRADA, utilizar ela para funcoes banais e um desuso a suas funcoes",
+                    "ERRADA, utilizar para acoes inuteis e um pessimo uso"
+
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Auxilio na resolucao de problemas complexos e diversas outras funcoes",
+                afirmacao: [
+                    "CERTA, devesse usala para o auxilio em problemas",
+                    "CERTA, usala de forma consiente e gratificante"
+                ],
+                proxima: 8,
+            },
+        ],
+    },
+    {
+        enunciado: "No meio academico qual o principal uso que atulmente os alunos tem usado as IA?",
+        alternativas: [
+            {
+                texto: "Para pesquisa e auxilio nos estudos?",
+                afirmacao: [
+                    "CERTA, Continue nos estudos",
+                    "CERTA, se empenhe e nao tenha medo de estudar"
+                ],
+                proxima: 9,
+            },
+            {
+                texto: "Fazendo com que elas facam suas avalaliacoes e respondendo suas atividades",
+                afirmacao: [
+                    "ERRADA, muitos usam para a criacao de projetos de forma preguicosa",
+                    "ERRADA, de maneira geral utilizar ela para projetos completamente feitos por ele e um desuso"
+                ],
+                proxima: 10,
+            },
+        ],
+    },
+];
